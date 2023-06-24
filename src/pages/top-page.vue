ce6fe87868bbadec6f0bbd12c15eea6f4e012fda
hiromi2525 <kingandprince0523hiromi@gmail.com>
2023-06-24 17:46:38 +0900

    トップ画面のタイトルカラー変更

--- src/pages/top-page.vue
+++ src/pages/top-page.vue
@@ -1,18 +1,16 @@
 <template>
-    <h1>Golf Facilty</h1>
-    <ul class="tab-menu-base">
+  <h1 class="green">Golf Facilty</h1>
+  <ul class="tab-menu-base">
     <li><router-link to="/signup">Singup</router-link></li>
     <li><router-link to="/login">Login</router-link></li>
     <li><router-link to="/addNew">ゴルフ場を追加する</router-link></li>
-   </ul>
-  </template>
-  
-  <script>
-  
-  </script>
-  
-  <style>
-  .red {
-    color: red;
-  }
-  </style>
+  </ul>
+</template>
+
+<script></script>
+
+<style>
+.green {
+  color: green;
+}
+</style>
